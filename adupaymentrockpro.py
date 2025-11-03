64043adfd84e7da6262d78b0b13c441256d2bba1
adupayment <aungaung101025@gmail.com>
2025-11-03 18:20:10 -0500

    Forcing WeasyPrint render fix

--- adupaymentrockpro.py
+++ adupaymentrockpro.py
@@ -636,6 +636,7 @@ class PlotlyChartManager:
                     '</head>', f'<style>{font_face_css}</style></head>', 1)
 
                 buffer = io.BytesIO()
+                # test comment
                 HTML(string=html_str).render().write_png(
                     buffer, presentational_hints=True)
                 buffer.seek(0)
